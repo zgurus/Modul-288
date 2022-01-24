b5314258722a1bd9426b8863fbfc7855946fccc7
zgurus <82454259+zgurus@users.noreply.github.com>
2022-01-24 19:00:21 +0100

    array

---
+++ array.js
@@ -0,0 +1,17 @@
+var me = {
+    FirstName: "Sujan",
+    LastName: "Gurung",
+    Lehrstelle: {
+        Job: "Mediamatiker",
+        Lehrfirma: "SIW",
+        Lehrdauer: "4 Jahre",
+        Schule: "BZZ"
+
+    }
+    };
+
+
+    var x;
+    for(x in me){
+        console.log(me[x]);
+    }
